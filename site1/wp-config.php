4df9b0a191e1914dca7db826f9b13619b33fa2a5
Behram CELEN <behramcelen@gmail.com>
2018-12-04 18:19:58 +0000

    last changes

--- site1/wp-config.php
+++ site1/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'testsite');
+define('DB_NAME', 'erdem_site_1');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'erdem');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'erdem');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
